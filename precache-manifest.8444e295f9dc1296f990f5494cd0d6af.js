d0150ace1765503ccec8e3467b391688047c1008
Vladimir Contreras <17748144+vlasscontreras@users.noreply.github.com>
2020-01-28 15:21:52 -0600

    Update build

---
+++ precache-manifest.8444e295f9dc1296f990f5494cd0d6af.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3fd157a15dda878d8a5e112c9e7513a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bd0f217831683af053e9",
+    "url": "./static/css/main.7077637e.chunk.css"
+  },
+  {
+    "revision": "033b9a1a23385bb5d4e7",
+    "url": "./static/js/2.b20483d6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.b20483d6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bd0f217831683af053e9",
+    "url": "./static/js/main.3728fae1.chunk.js"
+  },
+  {
+    "revision": "f321a6d3d2c5834fcf36",
+    "url": "./static/js/runtime-main.0b5b5306.js"
+  },
+  {
+    "revision": "b2a9d1fcdc4f7810d90d896f83a1a3dc",
+    "url": "./static/media/Iris.b2a9d1fc.svg"
+  },
+  {
+    "revision": "86e0c5a5d134c6192b75e96491ab8a18",
+    "url": "./static/media/Jax.86e0c5a5.svg"
+  }
+]);
